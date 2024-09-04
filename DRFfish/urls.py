b06f25fec21f0c279058e07bc9cbe2e5552e94e5
dengruizhe <2331602821@qq.com>
2024-09-05 00:34:13 +0800

    commit

--- DRFfish/urls.py
+++ DRFfish/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     # path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(
         "user_management/", include("user_management.urls", namespace="user_management")
     ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
