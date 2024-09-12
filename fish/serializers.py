6ac8d3bd2ab1ae433c17066608af2796248a9771
dengruizhe <2331602821@qq.com>
2024-09-13 00:07:33 +0800

    commit

--- fish/serializers.py
+++ fish/serializers.py
@@ -6,6 +6,7 @@ from .models import FishingPond
 class FishingPondSerializer(serializers.ModelSerializer):
     latitude = serializers.SerializerMethodField()
     longitude = serializers.SerializerMethodField()
+    fish_species = serializers.ListField(child=serializers.CharField(), required=False)
 
     class Meta:
         model = FishingPond
@@ -13,10 +14,10 @@ class FishingPondSerializer(serializers.ModelSerializer):
 
     def get_latitude(self, obj):
         if obj.location:
-            return obj.location.y  # 经度
+            return obj.location.y
         return None
 
     def get_longitude(self, obj):
         if obj.location:
-            return obj.location.x  # 纬度
+            return obj.location.x
         return None
